a326ef88c6e947c9d2e3ea6dfb4e204527e37519
Jan Trejbal <jan@trejbal.land>
2026-01-15 16:54:26 +0100

    Simplify serializer

--- src/DockerComposeBuilder/Converters/ServiceVolumeConverter.cs
+++ src/DockerComposeBuilder/Converters/ServiceVolumeConverter.cs
@@ -38,51 +38,7 @@ public class ServiceVolumeConverter : IYamlTypeConverter
             }
             else
             {
-                emitter.Emit(new MappingStart(AnchorName.Empty, TagName.Empty, false, MappingStyle.Block));
-
-                if (serviceVolume.Type != null)
-                {
-                    emitter.Emit(new Scalar("type"));
-                    emitter.Emit(new Scalar(AnchorName.Empty, TagName.Empty, serviceVolume.Type, ScalarStyle.DoubleQuoted, true, false));
-                }
-
-                if (serviceVolume.Source != null)
-                {
-                    emitter.Emit(new Scalar("source"));
-                    emitter.Emit(new Scalar(AnchorName.Empty, TagName.Empty, serviceVolume.Source, ScalarStyle.DoubleQuoted, true, false));
-                }
-
-                if (serviceVolume.Target != null)
-                {
-                    emitter.Emit(new Scalar("target"));
-                    emitter.Emit(new Scalar(AnchorName.Empty, TagName.Empty, serviceVolume.Target, ScalarStyle.DoubleQuoted, true, false));
-                }
-
-                if (serviceVolume.ReadOnly != null)
-                {
-                    emitter.Emit(new Scalar("read_only"));
-                    emitter.Emit(new Scalar(serviceVolume.ReadOnly.Value ? "true" : "false"));
-                }
-
-                if (serviceVolume.Bind != null)
-                {
-                    emitter.Emit(new Scalar("bind"));
-                    serializer(serviceVolume.Bind, typeof(ServiceVolumeBind));
-                }
-
-                if (serviceVolume.Volume != null)
-                {
-                    emitter.Emit(new Scalar("volume"));
-                    serializer(serviceVolume.Volume, typeof(ServiceVolumeVolume));
-                }
-
-                if (serviceVolume.Tmpfs != null)
-                {
-                    emitter.Emit(new Scalar("tmpfs"));
-                    serializer(serviceVolume.Tmpfs, typeof(ServiceVolumeTmpfs));
-                }
-
-                emitter.Emit(new MappingEnd());
+                serializer(value, type);
             }
         }
     }
